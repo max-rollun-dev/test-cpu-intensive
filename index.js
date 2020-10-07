f1d499a2968476bbeb82dec53829771d3a62c446
max_tricky_lake <mdovhopo@student.unit.ua>
2020-10-07 19:21:20 +0300

    add package.json

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 console.log('CPU intensive task');
 
 
-let hugeArray = [{date: '123123'}];
-while (true) {
-  hugeArray = hugeArray.concat(hugeArray)
-}
+const puppeteer = require('puppeteer');
 
+(async () => {
+  const foo = async () => {
+    console.log('start browser')
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    await page.screenshot({path: 'example.png'});
+  }
+
+  Promise.all([
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo(),
+    foo()
+  ])
+
+  // await browser.close();
+})();
 console.log('CPU task end');
