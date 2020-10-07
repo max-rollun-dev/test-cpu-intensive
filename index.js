fc04cfae01e001a13a9d94e017d61456062595f2
max-rollun-dev <54903558+max-rollun-dev@users.noreply.github.com>
2020-10-07 19:32:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 console.log('CPU intensive task');
 
+let n = 1;
+while(true) {
+  n += 1
+}
 
-const puppeteer = require('puppeteer');
 
-(async () => {
-  const foo = async () => {
-    console.log('start browser')
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://example.com');
-    await page.screenshot({path: 'example.png'});
-  }
-
-  Promise.all([
-    foo(),
-    foo(),
-    foo(),
-    foo()
-  ])
-
-  // await browser.close();
-})();
 console.log('CPU task end');
